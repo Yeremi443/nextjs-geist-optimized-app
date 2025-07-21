9c5c739167a634a51ac18b861ffdbbb3af93c41f
yeremy blanco belloo <125235118+Yeremi443@users.noreply.github.com>
2025-07-21 03:47:26 -0400

    Add .next/static/chunks/src_app_juego_page_tsx_a993527f._.js 1753084046478

---
+++ .next/static/chunks/src_app_juego_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_82ca6e50._.js",
+  "static/chunks/node_modules_ec63dfe8._.js"
+],
+    source: "dynamic"
+});
